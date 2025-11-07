489217014dc355f34da5360b93a31c5936ebed06
Drumming2008 <finn.reese@gmail.com>
2025-11-06 17:43:38 -0800

    ALL the fonts

--- js/settings.js
+++ js/settings.js
@@ -49,52 +49,80 @@ let settings = [
   }
 ]
 
-for (let i of settings) {
-  let settingWrapper = document.createElement("div")
-  settingWrapper.classList.add("setting-wrapper")
-  id("settings-content").append(settingWrapper)
-
-  let label = document.createElement("label")
-  label.innerHTML = `<h3>${i.label}</h3>`
-  settingWrapper.append(label)
-
-  let innerWrapper = document.createElement("div")
-  innerWrapper.classList.add("settings-inner-wrapper")
-  if (i.vertical) {
-    innerWrapper.style.flexDirection = "column"
-    innerWrapper.classList.add("vertical")
+queryLocalFonts().then(fonts => {
+  console.log("fonts", fonts)
+  for (let f of fonts) {
+    if (f.style != "Plain" && f.style != "Regular") continue
+    settings[1].buttons.push({
+      label: f.family,
+      labelStyle: `font-family: ${f.family};`,
+      value: f.family
+    })
   }
-  settingWrapper.append(innerWrapper)
+  loadSettings()
+})
 
-  if (i.type == "radio") {
-    settingWrapper.classList.add("setting-wrapper-radio")
-    for (let b of i.buttons) {
-      let label = document.createElement("label")
-      label.innerText = b.label
-      label.classList.add("settings-radio-label")
-      label.classList.add("secondary")
-      if (b.labelStyle) label.style.cssText = b.labelStyle
-      let button = document.createElement("input")
-      button.type = "radio"
-      button.value = b.value
-      button.name = i.label.toLowerCase()
 
-      button.onclick = () => {
-        window[i.function](b.value)
-        localStorage.setItem(i.label.toLowerCase(), b.value)
+
+// {
+//         label: "Open Dyslexic",
+//         labelStyle: "font-family: dyslexic-font, sans-serif;",
+//         value: "dyslexic-font, sans-serif"
+//       }
+
+
+
+function loadSettings() {
+  id("settings-content").innerHTML = ""
+  for (let i of settings) {
+    let settingWrapper = document.createElement("div")
+    settingWrapper.classList.add("setting-wrapper")
+    id("settings-content").append(settingWrapper)
+
+    let label = document.createElement("label")
+    label.innerHTML = `<h3>${i.label}</h3>`
+    settingWrapper.append(label)
+
+    let innerWrapper = document.createElement("div")
+    innerWrapper.classList.add("settings-inner-wrapper")
+    if (i.vertical) {
+      innerWrapper.style.flexDirection = "column"
+      innerWrapper.classList.add("vertical")
+    }
+    settingWrapper.append(innerWrapper)
+
+    if (i.type == "radio") {
+      settingWrapper.classList.add("setting-wrapper-radio")
+      for (let b of i.buttons) {
+        let label = document.createElement("label")
+        label.innerText = b.label
+        label.classList.add("settings-radio-label")
+        label.classList.add("secondary")
+        if (b.labelStyle) label.style.cssText = b.labelStyle
+        let button = document.createElement("input")
+        button.type = "radio"
+        button.value = b.value
+        button.name = i.label.toLowerCase()
+
+        button.onclick = () => {
+          window[i.function](b.value)
+          localStorage.setItem(i.label.toLowerCase(), b.value)
+        }
+
+        if (localStorage.getItem(i.label.toLowerCase())) {
+          if (localStorage.getItem(i.label.toLowerCase()) == b.value) button.click()
+        } else if (b.default) {
+          button.click()
+        }
+        
+        label.append(button)
+        innerWrapper.append(label)
       }
-
-      if (localStorage.getItem(i.label.toLowerCase())) {
-        if (localStorage.getItem(i.label.toLowerCase()) == b.value) button.click()
-      } else if (b.default) {
-        button.click()
-      }
-      
-      label.append(button)
-      innerWrapper.append(label)
     }
   }
 }
+
+loadSettings()
 
 function setTheme(value) {
   if (value == "light") document.documentElement.id = "light"
